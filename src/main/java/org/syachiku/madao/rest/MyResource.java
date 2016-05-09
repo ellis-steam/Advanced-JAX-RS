b23b01abd02994b5098cd10fc12dd816fd69cdcd
Ellis <ellis@s-team.com.tw>
2016-05-09 16:10:54 +0800

    Implemented MessageBodyWriter, access it from /webapi/test

--- src/main/java/org/syachiku/madao/rest/MyResource.java
+++ src/main/java/org/syachiku/madao/rest/MyResource.java
@@ -1,6 +1,8 @@
 package org.syachiku.madao.rest;
 
-import javax.inject.Singleton;
+import java.util.Calendar;
+import java.util.Date;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -8,7 +10,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-@Path("{pathParam}/test")
+@Path("test")
 public class MyResource {
 	
 	@PathParam("pathParam") private String pathParamExample;
@@ -16,7 +18,7 @@ public class MyResource {
 	
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	public String testMethod(){
-		return "It works! Path param used " + pathParamExample + " and Query Param used " + queryParamExample;
+	public Date testMethod(){
+		return Calendar.getInstance().getTime();
 	}
 }
