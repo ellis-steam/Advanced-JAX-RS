e7a79afc4f4dfebcceefb360030f3ba4bd6ee6bf
Ellis <ellis@s-team.com.tw>
2016-05-10 14:10:54 +0800

    Client improved in a more elegant way.

--- src/main/java/org/syachiku/madao/rest/client/RestApiClient.java
+++ src/main/java/org/syachiku/madao/rest/client/RestApiClient.java
@@ -2,6 +2,9 @@ package org.syachiku.madao.rest.client;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation.Builder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.syachiku.madao.messenger.model.Message;
@@ -12,9 +15,20 @@ public class RestApiClient {
 		
 		Client client = ClientBuilder.newClient();
 		
-		Response response = client.target("http://localhost:8080/advanced-jaxrs-01/webapi/messages/1").request().get();
-		Message message = response.readEntity(Message.class);
-		System.out.println(message.getMessage());
+		Message message = client.target("http://localhost:8080/advanced-jaxrs-01/webapi/messages/2")
+				.request(MediaType.APPLICATION_JSON)
+				.get(Message.class);
+		
+		/*
+		 * You can convert the retrieved content into String class, get the actual http payload, to debug, which is much less possible to go wrong.  
+		*/
+		/*
+		 * WebTarget target = client.target("http://localhost:8080/advanced-jaxrs-01/webapi/messages/1");
+		Builder builder = target.request();
+		Response response = builder.get();
+		*/
+		//Message message = response.readEntity(Message.class);
+		System.out.println(message);
 	}
 
 }
